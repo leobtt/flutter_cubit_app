4f2cce6d4710f5a13075d73fcac051a96d08e5d0
Leonardo Batista <leonarbat8@gmail.com>
2022-07-30 23:01:50 -0300

    feat: indexDots and button

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cubit/misc/colors.dart';
 import 'package:flutter_cubit/widgets/app_large_text.dart';
 import 'package:flutter_cubit/widgets/app_text.dart';
+import 'package:flutter_cubit/widgets/responsive_button.dart';
 
 class WelcomePage extends StatefulWidget {
   @override
@@ -34,6 +35,7 @@ class _WelcomePageState extends State<WelcomePage> {
               child: Container(
                 margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -56,7 +58,25 @@ class _WelcomePageState extends State<WelcomePage> {
                             size: 14,
                           ),
                         ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        ResponsiveButton(),
                       ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDots) {
+                        return Container(
+                            margin: const EdgeInsets.only(top: 2),
+                            width: 8,
+                            height: index == indexDots ? 25 : 8,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: index == indexDots
+                                  ? AppColors.mainColor
+                                  : AppColors.mainColor.withOpacity(0.3),
+                            ));
+                      }),
                     )
                   ],
                 ),
